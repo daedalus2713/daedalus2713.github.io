a6ab94b16b06a2ade6614172b334d58a0f613b35
daedalus2713 <75079751+daedalus2713@users.noreply.github.com>
2021-02-03 00:20:53 +0700

    Update genbook.sh

--- genbook.sh
+++ genbook.sh
@@ -2,6 +2,7 @@
 
 mkdir src;
 rsync -av --progress . ./src --exclude=docs/ --exclude=src/ --exclude=.gitignore --exclude=.git --exclude=genbook.sh --exclude=book.toml;
+sed -i "s/##/#/" src/SUMMARY.md
 mdbook build;
 rm -r src/;
 # mdbook serve;
