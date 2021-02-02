9199b574a2918edab4c8b0e16a4392294c9b55d1
Phan Phú Thành <thanhphanphu18@gmail.com>
2021-02-03 02:41:50 +0700

    Update genbook.sh

--- genbook.sh
+++ genbook.sh
@@ -4,5 +4,6 @@ mkdir src;
 rsync -av --progress . ./src --exclude=docs/ --exclude=src/ --exclude=.gitignore --exclude=.git --exclude=genbook.sh --exclude=book.toml;
 sed -i "s/##/#/" src/SUMMARY.md
 mdbook build;
+cp -r .gitbook/ docs/
 rm -r src/;
 # mdbook serve;
